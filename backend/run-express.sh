1b585c244bb51c6fe61ec1fa472edae59627b174
wiyan <wianwiryawan@gmail.com>
2025-07-02 11:56:14 +0700

    Add bash script for running backend and frontend

---
+++ backend/run-express.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Load NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
+
+# Use specific Node version
+nvm use lts/jod
+
+# Run your Next.js app
+dotenvx run -- node app.js
