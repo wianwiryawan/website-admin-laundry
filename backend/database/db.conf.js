aa5f902b49cfe4b7149b4c8d78a20d481bb07cb8
wiyan <wianwiryawan@gmail.com>
2025-07-02 20:52:15 +0700

    remove unused

--- backend/database/db.conf.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  host: 'localhost',
-  port: 5432,
-  database: 'my_database',
-  user: 'my_user',
-  password: 'my_password',
-};
--- backend/database/db.keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    DB_NAME: process.env.DB_NAME || 'my_db',
-    DB_USERNAME: process.env.DB_USERNAME || 'my_user',
-    DB_PASSWORD: process.env.DB_PASSWORD || 'my_password'
-}
