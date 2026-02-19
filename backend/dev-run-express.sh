d42dab4368cbdbdfe97120f2290a41a23e2e1ad6
wiyan <wianwiryawan@gmail.com>
2026-02-19 16:31:32 +0700

    Add .nvmrc and change command to only nvm use

--- backend/dev-run-express.sh
+++ backend/dev-run-express.sh
@@ -5,7 +5,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
 
 # Use specific Node version
-nvm use lts/jod
+nvm use
 
 # Run your Next.js app
 dotenvx run -- npx nodemon --watch . --ext ts,js --exec tsx app.ts
