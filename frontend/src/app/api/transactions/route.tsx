4df1f07d1ea584e2019aca756ff42a050fab82f7
wiyan <wianwiryawan@gmail.com>
2026-03-25 11:41:49 +0800

    Add API route for frontend

---
+++ frontend/src/app/api/transactions/route.tsx
@@ -0,0 +1,18 @@
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  //   const response = await fetch('https://localhost/api/users', {
+  //     // Optional: forward some headers, add auth tokens, etc.
+  //     headers: { Authorization: `Bearer ${process.env.API_TOKEN}` },
+  //   });
+
+  const response = await fetch("https://localhost:3002/api/transactions");
+
+  // Transform or forward the response
+  const data = await response.json();
+  const transformed = { ...data, source: "proxied-through-nextjs" };
+
+  return new Response(JSON.stringify(transformed), {
+    headers: { "Content-Type": "application/json" },
+  });
+}
