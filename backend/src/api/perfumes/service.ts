09ca7b29c606d5b3ea738aeb2392245b5baded0f
wianwiryawan <wianwiryawan@gmail.com>
2026-04-23 22:32:35 +0700

    refactor(query):Change in structure and validation

--- backend/src/api/perfumes/service.ts
+++ backend/src/api/perfumes/service.ts
@@ -1,73 +1,101 @@
 import { eq } from "drizzle-orm";
-import { db } from "../../database/drizzle/db";
-import { perfumesInData } from '../../database/drizzle/migrations/schema';
+import { db } from "@/database/drizzle/db";
+import { perfume } from '@/database/drizzle/schema';
 import { createPerfumeValidation, updatePerfumeValidation } from "./validation";
 
 // Services: Handle business logic and talk to the database.
+const now = () => new Date();
 
 export const getAllPerfumes = async () => {
-    return db.select().from(perfumesInData);
+    return db.select().from(perfume);
 };
 
 export const getPerfumeById = async (perfumeId: number) => {
-    return db.select().from(perfumesInData).where(
-        eq(perfumesInData.perfumes_id, perfumeId)
+    return db.select().from(perfume).where(
+        eq(perfume.perfumeId, perfumeId)
     );
 };
 
-export const addPerfume = async (perfumeData: unknown) => {
-    const result = createPerfumeValidation.safeParse(perfumeData);
+export const addPerfume = async (requestBody: unknown, adminId: number) => {
+    const result = createPerfumeValidation.safeParse(requestBody);
     if(!result.success){
-        // Handle validation error (throw, return, or log)
-        // throw new Error(`Validation failed: ${result.error}`);
+        throw result.error;
+    };
+
+    const validatedData = result.data;
+
+    const data = {
+        perfumeName: validatedData.perfumeName,
+        price: validatedData.price,
+        description: validatedData.description,
+        status: validatedData.status as 0 | 1 | 2,
+        createdBy: adminId,
+    };
+
+    // Use validated data
+    const inserted = await db
+        .insert(perfume)
+        .values(data);
+
+    return {
+        inserted,
+        message: "User created successfully"
+    };
+}
+
+export const updatePerfume = async (requestBody: unknown, adminId: number) => {
+    const result = updatePerfumeValidation.safeParse(requestBody);
+    if (!result.success) {
+        throw result.error;
+    };
+
+    const validatedData = result.data;
+
+    const data = {
+        perfumeName: validatedData.perfumeName,
+        price: validatedData.price,
+        description: validatedData.description,
+        status: validatedData.status as 0 | 1 | 2,
+        updatedDate: now(),
+        updatedBy: adminId,
+    };
+
+    const updated = await db
+        .update(perfume)
+        .set(data)
+        .where(
+            eq(perfume.perfumeId, validatedData.perfumeId)
+        );
+    
+    return {
+        updated,
+        message: "Data successfully updated"
+    }
+}
+
+export const deletePerfume = async (requestBody: unknown, adminId: number) => {
+    const result = updatePerfumeValidation.safeParse(requestBody);
+
+    if (!result.success) {
         throw result.error;
     }
 
     const validatedData = result.data;
 
-    // Use validated data
-    const inserted = await db
-        .insert(perfumesInData)
-        .values(validatedData)
-        .returning()
-    return {
-        message: "User created successfully",
-        data: inserted[0]
-    };
-}
-
-export const updatePerfumeById = async (perfumeId: number, perfumeData: unknown) => {
-    const result = updatePerfumeValidation.safeParse(perfumeData);
-    if (!result.success) {
-        throw result.error;
+    const data = {
+        status: 2 as 0 | 1 | 2,
+        updatedBy: adminId,
     }
 
-    const validatedData = {
-        ...result.data,
-        updated_date: new Date().toISOString() // Add current timestamp for updated_date
-    };
-
-    return db.update(perfumesInData)
-        .set(validatedData)
+    const deleted = await db
+        .update(perfume)
+        .set(data)
         .where(
-            eq(perfumesInData.perfumes_id, perfumeId)
+            eq(perfume.perfumeId, validatedData.perfumeId)
         );
-}
-
-export const softDeletePerfumeById = async (perfumeId: number, perfumeData: unknown) => {
-    const result = updatePerfumeValidation.safeParse(perfumeData);
-    if (!result.success) {
-        throw result.error;
+    
+    return {
+        deleted,
+        message: "Data successfully deleted"
     }
-
-    let validatedData = {
-        ...result.data,
-        updated_date: new Date().toISOString() // Add current timestamp for updated_date
-    };
-
-    return db.update(perfumesInData)
-        .set(validatedData)
-        .where(
-            eq(perfumesInData.perfumes_id, perfumeId)
-        ).returning();
 }
