405fecad0763072d4fef2198087ae5d9502f8523
wianwiryawan <wianwiryawan@gmail.com>
2026-04-23 22:31:37 +0700

    refactor(validation):Remove unused validation

--- backend/src/api/perfumes/validation.ts
+++ backend/src/api/perfumes/validation.ts
@@ -1,20 +1,13 @@
-import { z } from 'zod';
 import { createInsertSchema, createUpdateSchema } from 'drizzle-zod';
-import { perfumesInData } from '../../database/drizzle/migrations/schema';
+import { perfume } from '@/database/drizzle/schema';
+import z from 'zod';
 
 // Validation: Handle validation logic.
 
-const insertPerfumeSchema = createInsertSchema(perfumesInData);
-const updatePerfumeSchema = createUpdateSchema(perfumesInData);
+const insertSchema = createInsertSchema(perfume);
+const updateSchema = createUpdateSchema(perfume);
 
-export const createPerfumeValidation = insertPerfumeSchema.extend({
-    perfume_name: z.string().min(1).max(50),
-    description: z.string().min(1).max(100),
-});
-
-export const updatePerfumeValidation = updatePerfumeSchema.extend({
-    perfume_name: z.string().min(1).max(50).optional(),
-    price: z.string().min(1).optional(),
-    description: z.string().min(1).max(100).optional(),
-    status: z.int().min(0).max(1).optional(),
+export const createPerfumeValidation = insertSchema;
+export const updatePerfumeValidation = updateSchema.extend({
+    perfumeId: z.number(),
 });
