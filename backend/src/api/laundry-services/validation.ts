2531b67d3747f1fe056c13accc8394920fbce1dd
wianwiryawan <wianwiryawan@gmail.com>
2026-04-25 15:06:03 +0700

    refactor(validation):Simplify over validation

--- backend/src/api/laundry-services/validation.ts
+++ backend/src/api/laundry-services/validation.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 import { createInsertSchema } from 'drizzle-zod';
-import { laundryServicesInData } from '../../database/drizzle/migrations/schema';
+import { service } from '@/database/drizzle/schema';
 
 // Validation: Handle validation logic.
 
@@ -11,22 +11,11 @@ import { laundryServicesInData } from '../../database/drizzle/migrations/schema'
 // | `nullable` | string or `null`            | `undefined`, number   |
 // | `nullish`  | string, `null`, `undefined` | number, boolean, etc. |
 
-const insertLaundryServiceSchema = createInsertSchema(laundryServicesInData);
-const updateLaundryServiceSchema = createInsertSchema(laundryServicesInData);
+const insertLaundryServiceSchema = createInsertSchema(service);
+const updateLaundryServiceSchema = createInsertSchema(service);
 
-export const createLaundryServiceValidation = insertLaundryServiceSchema.extend({
-    service_name: z.string().min(1).max(100),
-    price: z.string()
-});
-
-export const updateLaundryServiceValidation = updateLaundryServiceSchema.extend({
-    service_name: z.string().min(1).max(100),
-    price: z.string(),
-    status: z.number(),
-    updated_date: z.preprocess(
-    (val) => {
-        if (typeof val === "string" || val instanceof Date) return new Date(val);
-    },
-    z.date()),
+export const createLaundryServiceValidation = insertLaundryServiceSchema;
+export const updateLaundryServiceValidation = updateLaundryServiceSchema.extend ({
+    serviceId: z.number(),
 });
 
